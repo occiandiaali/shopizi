9c17d60a1d37ff7e516093189b989ad3c651ecd4
occiandiaali <ocean.diaali@gmail.com>
2023-02-03 19:44:40 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,168 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LoadingController, Platform, ToastController } from '@ionic/angular';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  @ViewChild('video', { static: false }) video: ElementRef | undefined;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef | undefined;
+  // @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
 
-  constructor() {}
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult: string | URL | undefined;
+  loading: HTMLIonLoadingElement | null | undefined;
 
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform
+  ) {
+    // const isInStandaloneMode = () =>
+    //   'standalone' in window.navigator && window.navigator['platform'] === 'ios';
+    // if (this.plt.is('ios') && isInStandaloneMode()) {
+    //   console.log('I am an iOS PWA!');
+    //   // E.g. hide the scan functionality!
+    // }
+  }
+
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas?.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video?.nativeElement;
+  }
+
+  // Helper functions
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      //  message: `A weblink has been detected.\nOpen ${this.scanResult}?`,
+      message: `${this.scanResult}`,
+      position: 'bottom',
+      buttons: [
+        {
+          text: 'Open',
+          side: 'start',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          },
+        },
+        {
+          text: 'Dismiss',
+          role: 'cancel',
+          side: 'start',
+          handler: () => {
+            toast.dismiss();
+          },
+        },
+      ],
+    });
+    toast.present();
+  }
+
+  async startScan() {
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' },
+    });
+
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert',
+      });
+
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  reset() {
+    this.scanResult = undefined;
+  }
+
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  /**
+   * Fallback for iOS issues or just handle a previously
+   * captured image
+   * 
+   * captureImage() {
+  this.fileinput.nativeElement.click();
+}
+
+handleFile(files: FileList) {
+  const file = files.item(0);
+
+  var img = new Image();
+  img.onload = () => {
+    this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+    const imageData = this.canvasContext.getImageData(
+      0,
+      0,
+      this.canvasElement.width,
+      this.canvasElement.height
+    );
+    const code = jsQR(imageData.data, imageData.width, imageData.height, {
+      inversionAttempts: 'dontInvert'
+    });
+
+    if (code) {
+      this.scanResult = code.data;
+      this.showQrToast();
+    }
+  };
+  img.src = URL.createObjectURL(file);
+}
+   * 
+   */
 }
